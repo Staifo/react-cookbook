89c8f3524e7ddd927131de7a11cd26540ce8ec13
Staifo <69309608+Staifo@users.noreply.github.com>
2020-10-15 09:41:14 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState, useEffect, Fragment } from "react";
+import { client } from "./components/client";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Recipes from "./components/Recipes";
+import Uganda from "./components/Uganda";
+import { Link, Route, Router, Switch } from "react-router-dom";
+import NewRecipe from "./components/Recipe";
+import Nigeria from "./components/Nigeria";
+import Anatolia from "./components/Anatolia";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import "./App.css";
+
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    client.getEntries().then((response) => {
+      setRecipes(response.items);
+      console.log(response.items[0].fields); //only Object {description, ingredients,... }...inside fields
+    });
+  }, []);
+
+  return (
+    <div className="root">
+      <div className="recipes">
+      
+        <Header/>
+
+        <div className="inputDiv">
+          {/*<nav className="navbar navbar-nav navbar-expand-lg navbar-light bg-light" id="bg-light">
+      </nav>*/}
+          <input type="search" placeholder="Search" />
+          <Link className="Link" to="/Recipes">
+            Home
+          </Link>
+          <Link className="Link" to="/Recipes/Uganda">
+            Welcome to: Uganda
+          </Link>
+          <Link className="Link" to="/Recipes/Nigeria">
+            Welcome to: Nigeria
+          </Link>
+          <Link className="Link" to="/Recipes/Anatolia">
+            Welcome to: Anatolia
+          </Link>
+        </div>
+        <Switch>
+         <Route path="/Recipes/:country?">
+            <Recipes recipes={recipes} />
+          </Route>
+        {/*  <Route path='/Recipes/Home'>
+          <Recipes recipes={recipes} />
+          </Route>
+         <Route path="/Recipes/Uganda">
+           <Uganda/>
+         </Route>
+          <Route path="/Recipes/Nigeria">
+            <Nigeria />
+          </Route>
+          <Route path="/Recipes/Anatolia">
+            <Anatolia/>
+          </Route>*/}
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import Uganda from "./Uganda";
+import { Link, Route, Switch } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header = () => {
+
+
+  return (
+    <div className="header">
+      <Link style={{textDecoration:'none'}} className="LinkDaily" to="/Recipes">
+        <div className="logo">
+          <h1 className="h1">
+            <span className="daily">
+              <em>Daily</em>
+            </span>
+            <span className="cious">
+              <em>cious</em>
+            </span>
+          </h1>
+        </div>
+      </Link>
+    </div>
+  );
+};
+export default Header;
